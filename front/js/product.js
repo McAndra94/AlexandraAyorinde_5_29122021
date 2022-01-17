d37571edd5272c3eab99d164b57c910afa178e24
Alexandra <91855591+McAndra94@users.noreply.github.com>
2022-01-17 20:31:33 +0100

    Update product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,17 +1,42 @@
-/* 
+const UrlSP = new URLSearchParams(window.location.search);
+const idProduct = UrlSP.get("id");
+console.log(idProduct);
 
-${element[1]._id}
-${element[1].imageUrl}
-${element[1].altTxt}
-title : ${element[1].name}
-price : ${element[1].price}
-description : ${element[1].description}
-colors : ${element[1].colors}
-
-*/
-
-let api = fetch("http://localhost:3000/api/products" + idProduct)
+let api = fetch("http://localhost:3000/api/products/" + idProduct)
 .then(
-    async result =>{
-        let response = await result.json();}
+    async result => {
+        let response = await result.json();
+        console.log(response);
+
+        let productImg = document.createElement("img");
+        document.querySelector(".item__img").appendChild(productImg);
+        productImg.src = response.imageUrl;
+        productImg.alt = response.altTxt;
+
+        const productName = document.getElementById("title");
+        productName.innerHTML = response.name;
+
+        const productPrice = document.getElementById("price");
+        productPrice.innerHTML = response.price;
+
+        const productDescription = document.getElementById("description");
+        productDescription.innerHTML = response.description;
+
+        let colors = response.colors;
+        let selectColor = document.getElementById("colors");
+        colors.forEach((color)=>{
+            let option = document.createElement("option");
+            option.value = color
+            option.innerHTML = color
+            selectColor.appendChild(option)
+        })
+    }
 )
+
+/*
+document.getElementById("addToCart").addEventListener("click", (event)=>{
+    if (value > 0){
+        let
+    }
+};
+*/
